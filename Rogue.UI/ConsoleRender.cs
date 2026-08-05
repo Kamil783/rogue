528f7cd38691e84186f67e35d288663648275d2d
Kamil <galyamov.kamil5@gmail.com>
2026-08-05 20:38:15 +0300

    fix

--- Rogue.UI/ConsoleRender.cs
+++ Rogue.UI/ConsoleRender.cs
@@ -158,7 +158,7 @@ namespace Rogue.UI
         {
             CellType.Wall => '#',
             CellType.Floor => '.',
-            CellType.Corridor => '#',
+            CellType.Corridor => '!',
             CellType.Exit => '>',
             _ => ' '
         };
